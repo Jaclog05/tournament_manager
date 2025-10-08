fcc4ef176ba513d3c4bc93a87417b73fd37d53ba
Jaider <jaidercueto.1010@gmail.com>
2025-10-08 11:58:47 -0500

    Añadiendo rutas de jugadores al servidor principal de express

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const tournamentRoutes = require('./routes/tournamentRoutes');
 const teamRoutes = require('./routes/teamRoutes');
+const playerRoutes = require('./routes/playerRoutes');
 require('dotenv').config();
 
 const app = express();
@@ -19,6 +20,7 @@ app.get('/api/health', (req, res) => {
 app.use('/api/auth', authRoutes);
 app.use('/api/tournaments', tournamentRoutes);
 app.use('/api/tournaments', teamRoutes);
+app.use('/api/tournaments', playerRoutes);
 
 const PORT = process.env.PORT || 3000;
 
