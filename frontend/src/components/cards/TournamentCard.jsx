fc42501952b870d29f8261ca192ee6b3a034c946
Jaider <jaidercueto.1010@gmail.com>
2025-11-28 18:24:11 -0500

    Creando vista Dashboard. Creando Cards components

---
+++ frontend/src/components/cards/TournamentCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { FaTrophy } from "react-icons/fa";
+
+function TournamentCard({tournamentName, teamsAmount}) {
+  return (
+    <div
+      className="card d-flex flex-row justify-content-start align-items-center gap-2 px-3"
+      style={{width: '180px'}}
+    >
+      <FaTrophy style={{fontSize: '24px'}}/>
+      <div className='d-flex flex-column gap-1' style={{fontSize: '12px'}}>
+        <p className='mb-0' style={{fontWeight: 'bold'}}>{tournamentName}</p>
+        <p className='mb-0'>{teamsAmount} teams</p>
+      </div>
+    </div>
+  )
+}
+
+export default TournamentCard
