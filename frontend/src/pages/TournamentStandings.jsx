9ad9298d1aee323c465ac136e350f887a05b2b60
Jaider <jaidercueto.1010@gmail.com>
2025-12-17 07:58:16 -0500

    Refactoring Tournament Standings view

--- frontend/src/pages/TournamentStandings.jsx
+++ frontend/src/pages/TournamentStandings.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react';
-import StandingsTable from '../components/standingsTable/StandingsTable';
 import { tournamentService } from '../services/api';
 import { useParams } from 'react-router-dom';
+import TournamentHeader from '../components/TournamentHeader';
+import StandingsTable from '../components/standingsTable/StandingsTable';
 
 const App = () => {
   const { id } = useParams();
@@ -21,17 +22,17 @@ const App = () => {
   }, []);
 
   return (
-    <div className='vh-100 w-100 d-flex flex-column px-5 gap-3' style={{paddingTop: '75px'}}>
-      <div className="mb-4 d-flex flex-column align-items-center gap-2">
-        <h2 className="fw-bold text-center">Tabla de Posiciones</h2> {/* TODO: dynamic name */}
-        <h2 className="fw-bold text-center">Liga de Prueba Predictiva 2024</h2> {/* TODO: dynamic name */}
-        <div className="d-flex align-items-center justify-content-center gap-2">
-          <span className="text-muted">
-            Enero 15 - Febrero 15 {/* TODO: dynamic date */}
-          </span>
-        </div>
-        <span className="badge bg-success text-center">En curso</span> {/* TODO: dynamic status */}
-      </div>
+    <div
+      className='vh-100 w-100 d-flex flex-column px-5 gap-3'
+      style={{paddingTop: '75px'}}
+    >
+      <TournamentHeader
+        section="Tabla de Posiciones" /* TODO: dynamic name */
+        tournamentName="Liga de Prueba Predictiva 2024" /* TODO: dynamic name */
+        startDate="2024-01-01T00:00:00.000Z" /* TODO: dynamic date */
+        endDate="2024-12-31T00:00:00.000Z" /* TODO: dynamic date */
+        status="En curso" /* TODO: dynamic status */
+      />
       <StandingsTable data={data} />
     </div>
   );
