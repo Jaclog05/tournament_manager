14be95b5f69e5f274d80b278c89c93d732a15f22
Jaider <jaidercueto.1010@gmail.com>
2025-12-17 07:31:22 -0500

    Fixing dateFormatter. Using UTC time instead of Local Timezone

--- frontend/src/utils/dateFormatter.js
+++ frontend/src/utils/dateFormatter.js
@@ -1,14 +1,14 @@
 const formatDateTime = (dateString, includeTime = false) => {
   const date = new Date(dateString);
-  const day = date.getDate();
-  const month = date.toLocaleString('es-ES', { month: 'long' });
+  const day = date.getUTCDate();
+  const month = date.toLocaleString('es-ES', { month: 'long', timeZone: 'UTC' });
   const formattedMonth = month.charAt(0).toUpperCase() + month.slice(1);
 
   let formattedDate = `${day} ${formattedMonth}`;
 
   if(includeTime) {
-    const hours = date.getHours().toString().padStart(2, '0');
-    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const hours = date.getUTCHours().toString().padStart(2, '0');
+    const minutes = date.getUTCMinutes().toString().padStart(2, '0');
     formattedDate += ` - ${hours}:${minutes}`
   }
   return formattedDate;
